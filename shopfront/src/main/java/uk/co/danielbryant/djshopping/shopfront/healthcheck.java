80872476c63493a7ed37e0dc4df3e75a9552f5c0
M Robel Miah <47643759+mrobel@users.noreply.github.com>
2023-12-01 23:04:01 +0100

    Create healthcheck.java

---
+++ shopfront/src/main/java/uk/co/danielbryant/djshopping/shopfront/healthcheck.java
@@ -0,0 +1,13 @@
+package uk.co.danielbryant.djshopping.shopfront;
+ 
+import java.util.Date;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+public class healthcheck {
+    @GetMapping("/healthcheckSF")
+    public String healthcheck() {
+        return "Hello User this response from ShopFront !! " + new Date();
+    }
+}
