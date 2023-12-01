215d08906868241e12e3e42365ff7ceb879e300c
M Robel Miah <47643759+mrobel@users.noreply.github.com>
2023-12-01 22:57:59 +0100

    Update healthcheck.java

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
@@ -11,3 +11,16 @@
 //         return "Hello User !! " + new Date();
 //     }
 // }
+package uk.co.danielbryant.djshopping.stockmanager;
+ 
+import java.util.Date;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+public class healthcheck {
+    @GetMapping("/healthcheckSM")
+    public String healthcheck() {
+        return "Hello User this response from Stock Manager !! " + new Date();
+    }
+}
